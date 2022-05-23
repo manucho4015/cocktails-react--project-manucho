b08a34f0d14348b6f91f0abb031d9fc9f7589e71
Emmanuel Ochieng <eochieng16@gmail.com>
2022-05-23 15:47:58 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Corrupti, reprehenderit. Est porro, quos distinctio aliquid
+        culpa iste explicabo eius accusantium maxime, beatae vero error
+        quo cupiditate asperiores quaerat ipsum pariatur tempora repellendus
+        atque placeat. Quibusdam consectetur excepturi accusamus ratione nostrum!
+      </p>
+    </section>
+  )
+}
+
+export default About
